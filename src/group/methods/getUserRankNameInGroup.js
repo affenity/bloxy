7b66c78a4d8640c7d539b07bb2fde9e9841156f6
joritochip <jojobuddyr@gmail.com>
2018-11-07 19:33:35 -0700

    Added getUserRankNameInGroup. function
    Returns the role/rank name of a specified user in a specified group.
    "setup" has two arguments: groupId and userId
    **NOTE: Method name may be slightly wordy but I wanted to make sure it fits with the other method names**

---
+++ src/group/methods/getUserRankNameInGroup.js
@@ -0,0 +1,15 @@
+
+
+const getUserGroups = require('../../client/methods/getUserGroups');
+
+
+module.exports = async function (setup, self) {
+    let newPromise = new Promise(async function(resolve, reject) {
+        getUserGroups(setup.userId, self).then(userGroups=>{
+            let getGroup = userGroups.find(x=>x.group.groupId==setup.groupId)
+            let userRank = getGroup.userRole
+            resolve(userRank);
+        }).catch(reject);
+    })
+    return newPromise;
+}
