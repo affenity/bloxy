be7d5f8cace01eb28c0577a0c519ece9eb5cabcb
Martin <m@xnx.no>
2019-12-14 18:45:44 +0100

    Implemented coverage testing

--- tests/internals.test.js
+++ tests/internals.test.js
@@ -1,5 +1,9 @@
+/* eslint-disable */
 const config = require("./config");
 
 const bloxy = require("../index");
 const c1 = new bloxy();
-const c2 = new bloxy.Client();
+
+test("Checks if you can construct bloxy with just calling it", () => {
+	expect(c1).toBeInstanceOf(bloxy);
+});
