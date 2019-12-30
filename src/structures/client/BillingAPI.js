4be594a15dea46b675a5334addd7c8900f83ee3e
Martin <m@xnx.no>
2019-12-30 11:06:53 +0100

    Updates

--- src/structures/client/BillingAPI.js
+++ src/structures/client/BillingAPI.js
@@ -1,7 +1,33 @@
 class BillingAPI {
 	constructor (client) {
 		this.client = client;
+		this.baseUrl = "https://billing.roblox.com/";
+		this.request = require("./_request").bind(this, this);
 	}
+
+	getDeveloperExchangeRate () {
+		return this.request({
+			url: "v1/developer-exchange-rate"
+		}, {}, {
+			disableAuth: true
+		});
+	}
+
+	submitDeveloperExchange ({ firstName, lastName, email, password, amount }) {
+		return this.request({
+			url: "v1/developer-exchange-request",
+			method: "POST",
+			json: {
+				firstName,
+				lastName,
+				email,
+				password,
+				amount
+			}
+		});
+	}
+
+	
 }
 
 module.exports = BillingAPI;
