1f1dada6ddcd82a9678d1044b37a6528277199bd
Martin <m@xnx.no>
2020-01-01 15:59:05 +0100

    Updates

--- src/structures/client/GameInternationalizationAPI.js
+++ src/structures/client/GameInternationalizationAPI.js
@@ -1,6 +1,92 @@
 class GameInternationalizationAPI {
 	constructor (client) {
 		this.client = client;
+		this.baseUrl = "https://gameinternationalization.roblox.com/";
+		this.request = require("./_request").bind(this, this);
+	}
+
+	getGameNameDescription (universeId) {
+		return this.request({
+			url: `v1/name-description/games/${universeId}`
+		}, {}, {
+			disableAuth: true
+		});
+	}
+
+	updateGameNameDescription ({ universeId, data }) {
+		return this.request({
+			url: `v1/name-description/games/${universeId}`,
+			method: "PATCH",
+			json: {
+				data
+			}
+		});
+	}
+
+	getNameDescriptionMetadata () {
+		return this.request({
+			url: "v1/name-description/metadata"
+		}, {}, {
+			disableAuth: true
+		});
+	}
+
+	getGameSourceLanguage (universeId) {
+		return this.request({
+			url: `v1/source-language/games/${universeId}`
+		}, {}, {
+			disableAuth: true
+		});
+	}
+
+	setGameSourceLanguage ({ universeId, languageCode }) {
+		return this.request({
+			url: `v1/source-language/games/${universeId}`,
+			method: "PATCH",
+			qs: {
+				languageCode
+			}
+		});
+	}
+
+	getGameSupportedLanguages (universeId) {
+		return this.request({
+			url: `v1/supported-languages/games/${universeId}`
+		}, {}, {
+			disableAuth: true
+		});
+	}
+
+	updateGameSupportedLanguages ({ universeId, languages }) {
+		return this.request({
+			url: `v1/supported-languages/games/${universeId}`,
+			method: "PATCH",
+			json: languages
+		});
+	}
+
+	getAutomaticTranslationStatus (universeId) {
+		return this.request({
+			url: `v1/supported-languages/games/${universeId}/automatic-translation-status`
+		});
+	}
+
+	updateAutomaticTranslationStatus ({ universeId, languageCode, enabled }) {
+		return this.request({
+			url: `v1/supported-languages/games/${universeId}/languages/${languageCode}/automatic-translation-status`,
+			method: "PATCH",
+			json: {
+				enableAutomaticTranslation: enabled
+			}
+		});
+	}
+
+	getSupportedLanguagesMetadata () {
+		return this.request({
+			url: "v1/supported-languages/metadata"
+		}, {}, {
+			disableAuth: true
+		});
 	}
 }
 
--- src/structures/client/GamesAPI.js
+++ src/structures/client/GamesAPI.js
@@ -1,6 +1,77 @@
 class GamesAPI {
 	constructor (client) {
 		this.client = client;
+		this.baseUrl = "https://games.roblox.com/";
+		this.request = require("./_request").bind(this, this);
+	}
+
+	getGamesInfo (gameIds) {
+		return this.request({
+			url: "v1/games",
+			qs: {
+				universeIds: gameIds.join("\n")
+			}
+		}, {}, {
+			disableAuth: true
+		});
+	}
+
+	getGameServers ({ placeId, serverType, sortOrder, limit, cursor }) {
+		return this.request({
+			url: `v1/games/${placeId}/servers/${serverType}`,
+			qs: {
+				sortOrder,
+				limit,
+				cursor
+			}
+		}, {}, {
+			disableAuth: true
+		});
+	}
+
+	getGamesProductInfo (universeIds) {
+		return this.request({
+			url: "v1/games/games-product-info",
+			qs: {
+				universeIds: universeIds.join("\n")
+			}
+		}, {}, {
+			disableAuth: true
+		});
+	}
+
+	listGames (options) {
+		const queries = {};
+		Object.keys(options).map(k => {
+			queries[`model.${k}`] = options[k];
+		});
+
+		return this.request({
+			url: "v1/games/list",
+			qs: queries
+		}, {}, {
+			disableAuth: true
+		});
+	}
+
+	getPlacesInfo (placeIds) {
+		return this.request({
+			url: "v1/games/multiget-place-details",
+			qs: {
+				placeIds: placeIds.join("\n")
+			}
+		}, {}, {
+			disableAuth: true
+		});
+	}
+
+	getGamesPlayabilityStatus (universeIds) {
+		return this.request({
+			url: "v1/games/multiget-playability-status",
+			qs: {
+				universeIds: universeIds.join("\n")
+			}
+		});
 	}
 }
 
