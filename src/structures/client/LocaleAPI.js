da94182e87d9cc68f56449b4c5eef179b8570f6d
Martin <m@xnx.no>
2020-01-02 13:20:17 +0100

    Updates

--- src/structures/client/LocaleAPI.js
+++ src/structures/client/LocaleAPI.js
@@ -1,7 +1,73 @@
 class LocaleAPI {
 	constructor (client) {
 		this.client = client;
+		this.baseUrl = "https://locale.roblox.com/";
+		this.request = require("./_request").bind(this, this);
 	}
+
+	getCountriesByLocale (locale) {
+		return this.request({
+			url: "v1/country-regions",
+			qs: {
+				locale
+			}
+		});
+	}
+
+	getSelfCountry () {
+		return this.request({
+			url: "v1/country-regions/user-country-region"
+		});
+	}
+
+	setSelfCountry (countryId) {
+		return this.request({
+			url: "v1/country-regions/user-country-region",
+			method: "PATCH",
+			json: {
+				countryId
+			}
+		})
+	}
+
+	getLocales (displayLocaleValue) {
+		return this.request({
+			url: "v1/locales",
+			qs: {
+				displayValueLocale: displayLocaleValue
+			}
+		});
+	}
+
+	getSupportedLocales () {
+		return this.request({
+			url: "v1/locales/supported-locales"
+		})
+	}
+
+	getSelfLocale () {
+		return this.request({
+			url: "v1/locales/user-locale"
+		})
+	}
+
+	getLocusSupportedLocales () {
+		return this.request({
+			url: "v1/locales/user-localization/locus-supported-locales"
+		})
+	}
+
+	setSelfSupportedLocale (localeCode) {
+		return this.request({
+			url: "v1/locales/set-user-supported-locale",
+			method: "POST",
+			json: {
+				supportedLocaleCode: localeCode
+			}
+		});
+	}
+
+
 }
 
 module.exports = LocaleAPI;
