55f3aa6f56641786d76138a05661ff81f7b7a034
guidojw <guidojwdejong@hotmail.com>
2020-03-27 03:02:45 +0100

    Always refresh token

--- src/controllers/Rest/src/token/TokenController.js
+++ src/controllers/Rest/src/token/TokenController.js
@@ -5,16 +5,12 @@ class RestTokenController {
 	constructor (client) {
 		this.client = client;
 		this.token = null;
-		this.fetchInterval = null;
-		this.refreshInterval = 5 * 60 * 1000;
 
 		this.refresh = require("./src/refresh").bind(this, this);
 	}
 
 	async fetch () {
-		if (!this.token) {
-			this.token = await this.refresh();
-		}
+		this.token = await this.refresh();
 		return this.token;
 	}
 }
