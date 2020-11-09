e02b4120532b55c1f9a60e795eef679266f767c6
unknown <m@xnx.no>
2020-11-09 10:40:46 +0100

    Added test for DevelopAPI.createDeveloperProduct to ensure it's working

---
+++ tests/apis/DevelopAPI.js
@@ -0,0 +1,39 @@
+const TEST_COOKIE = require("../getCookie");
+const bloxy = require("../../dist");
+const faker = require("faker");
+const client = new bloxy.Client({
+    credentials: {
+        cookie: TEST_COOKIE
+    }
+});
+const { wait } = require("../util");
+
+beforeAll(async () => {
+    return client.login(
+        TEST_COOKIE
+    )
+        .then(clientUser => {
+            console.log(`Using test account ${clientUser.id} with name ${clientUser.name}`);
+        });
+});
+
+describe("testing DevelopAPI", function () {
+    it("should create a developer product with no issue", function () {
+        const createdProduct = client.apis.developAPI.createDeveloperProduct({
+            universeId: 2021554667,
+            priceInRobux: 10,
+            name: `Test - music ${Date.now() / 1000}`,
+            iconImageAssetId: 5874994712,
+            description: `A developer product automatically created using bloxy at ${new Date().toISOString()}`
+        });
+
+        expect(createdProduct)
+            .resolves
+            .toMatchObject({
+                name: String
+            });
+        expect(createdProduct)
+            .resolves
+            .toHaveProperty(["id", "name", "Description", "shopId", "iconImageAssetId"]);
+    });
+});
