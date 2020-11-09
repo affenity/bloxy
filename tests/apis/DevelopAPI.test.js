577253c35f92f10103152da0a137787f713d1b7a
unknown <m@xnx.no>
2020-11-09 15:35:44 +0100

    Updated DevelopAPI.test.js with more tests

--- tests/apis/DevelopAPI.test.js
+++ tests/apis/DevelopAPI.test.js
@@ -18,17 +18,129 @@ beforeAll(async () => {
 });
 
 describe("testing DevelopAPI", function () {
-    it("should create a developer product with no issue", async function () {
-        const createdProduct = await client.apis.developAPI.createDeveloperProduct({
-            universeId: 2021554667,
-            priceInRobux: 10,
-            name: `Test - music ${Date.now() / 1000}`,
-            iconImageAssetId: 5874994712,
-            description: `A developer product automatically created using bloxy at ${new Date().toISOString()}`
+    describe("testing universe-related APIs", async () => {
+        const firstUniverseFound = await client.apis.developAPI.getSelfUniverses({
+            sortOrder: "Desc",
+            limit: 10
         })
-            .catch(e => e);
+            .then(response => response.data[0])
+            .catch(e => {
+                throw new Error(`Failed to retrieve first universe! ${e}`);
+            });
 
-        return expect(createdProduct)
-            .toHaveProperty(["id", "name", "Description", "shopId", "iconImageAssetId"]);
+        it(`should create a developer product in the first universe: ${firstUniverseFound.id}`, async function () {
+            const createdProduct = await client.apis.developAPI.createDeveloperProduct({
+                universeId: 2021554667,
+                priceInRobux: 10,
+                name: `Test - music ${Date.now() / 1000}`,
+                iconImageAssetId: 5874994712,
+                description: `A developer product automatically created using bloxy at ${new Date().toISOString()}`
+            })
+                .catch(e => e);
+
+            return expect(createdProduct)
+                .toHaveProperty(["id", "name", "Description", "shopId", "iconImageAssetId"]);
+        });
+
+        it("should deactivate the universe", function () {
+            return expect(
+                client.apis.developAPI.deactivateUniverse({
+                    universeId: firstUniverseFound.id
+                })
+            )
+                .resolves
+                .not
+                .toThrow();
+        });
+
+        it("should activate the universe", function () {
+            return expect(
+                client.apis.developAPI.activateUniverse({
+                    universeId: firstUniverseFound.id
+                })
+            )
+                .resolves
+                .not
+                .toThrow();
+        });
+        it("should filter \"fuck\"", function () {
+            return expect(
+                client.apis.developAPI.filterGameUpdateNotificationText({
+                    text: `"fuck"`
+                })
+            )
+                .resolves
+                .toMatchObject({
+                    filteredGameUpdateText: "####",
+                    isFiltered: true,
+                    moderationLevel: 3
+                });
+        });
+        it("should not filter", function () {
+            return expect(
+                client.apis.developAPI.filterGameUpdateNotificationText({
+                    text: "roblox"
+                })
+            )
+                .resolves
+                .toMatchObject({
+                    filteredGameUpdateText: "roblox",
+                    isFiltered: false,
+                    moderationLevel: 1
+                });
+        });
+        it("should get places in the universe found and root place id should be in one of them", function () {
+            return expect(
+                client.apis.developAPI.getPlacesInUniverse({
+                    universeId: firstUniverseFound.id
+                })
+            )
+                .resolves
+                .toMatchObject({
+                    data: [
+                        {
+                            id: firstUniverseFound.rootPlaceId,
+                            universeId: firstUniverseFound.id,
+                            name: firstUniverseFound.name,
+                            description: firstUniverseFound.description
+                        }
+                    ]
+                });
+        });
+        it("should test place statistics", function () {
+            return expect(
+                client.apis.developAPI.getPlaceStatistics({
+                    placeId: firstUniverseFound.rootPlaceId
+                })
+            )
+                .resolves
+                .toMatchObject({
+                    placeId: firstUniverseFound.rootPlaceId,
+                    dataType: 0,
+                    dataGranularity: 0,
+                    data: {
+                        Total: {
+                            type: "Total",
+                            data: {}
+                        }
+                    }
+                });
+        });
+        it("should retrieve root place compatibilities", function () {
+            return expect(
+                client.apis.developAPI.getPlaceCompatibilities({
+                    placeId: firstUniverseFound.rootPlaceId
+                })
+            )
+                .resolves
+                .toMatchObject({
+                    Compatibilities: [
+                        {
+                            status: "Unknown",
+                            platformName: "iPhone 11 Pro Max"
+                        }
+                    ]
+                });
+        });
     });
 });
