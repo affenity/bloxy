7e684cf7f93040783d73216b668ad7e6a6b70465
Martin <m@xnx.no>
2021-02-21 16:34:01 +0100

    Added forgotten parameter when constructing User class

--- src/client/Client.ts
+++ src/client/Client.ts
@@ -15,37 +15,37 @@ export default class Client extends ClientBase {
     public socket: ClientSocket.Socket;
     public dataStoreManager: DataStoreManager;
     public chat: ChatManager;
-
+    
     constructor (options?: ClientOptions) {
         super(options);
-
+        
         this.user = null;
         this.apis = initAPIs(this);
         this.rest = new RESTController(this, this.options.rest);
         this.socket = new ClientSocket.Socket(this);
         this.dataStoreManager = new DataStoreManager(this);
         this.chat = new ChatManager(this);
-
+        
         this.init();
     }
-
+    
     public isLoggedIn (): boolean {
         return this.user !== null;
     }
-
+    
     init (): void {
         if (this.options.rest) {
             this.rest.setOptions(this.options.rest);
         }
     }
-
+    
     public async login (cookie?: string): Promise<ClientUser> {
         this.log("info", {
             name: "Client.login",
             description: `Started login process..`
         });
         cookie = cookie || (this.options.credentials || {}).cookie || undefined;
-
+        
         if (!cookie) {
             throw new Error("Attempted to log in without a cookie!");
         }
@@ -53,7 +53,7 @@ export default class Client extends ClientBase {
             ...this.options.credentials,
             cookie
         };
-
+        
         const createdCookie = this.rest.createCookie({
             key: ".ROBLOSECURITY",
             value: cookie,
@@ -61,29 +61,29 @@ export default class Client extends ClientBase {
             hostOnly: false,
             httpOnly: false
         });
-
+        
         this.rest.addCookie(createdCookie);
-
+        
         this.log("info", {
             name: "Client.login",
             description: `Added cookie to cookie jar, proceeding to fetching authenticated user information..`
         });
-
+        
         const getAuthenticationData = await this.apis.usersAPI.getAuthenticatedUserInformation();
         this.user = new ClientUser({
             id: getAuthenticationData.id,
             name: getAuthenticationData.name
         }, this);
         this.emit("loggedIn");
-
+        
         this.log("info", {
             name: "Client.login",
             description: `Successfully logged in as ${getAuthenticationData.name}`
         });
-
+        
         return this.user;
     }
-
+    
     getGroup (groupId: number): Promise<Group> {
         return this.apis.groupsAPI.getGroup({
             groupId
@@ -96,18 +96,19 @@ export default class Client extends ClientBase {
                 }
             });
     }
-
+    
     getUser (userId: number | string): Promise<User> {
         if (typeof userId === "string") {
             userId = parseInt(userId);
         }
-
+        
         return this.apis.otherAPI.getUserProfileHeader({
             userId
         })
             .then(data => new User({
                 id: data.ProfileUserId,
                 name: data.ProfileUserName,
+                displayName: data.ProfileDisplayName,
                 canFollow: data.CanBeFollowed,
                 canSeeInventory: data.CanSeeInventory,
                 canTrade: data.CanTrade,
@@ -138,7 +139,7 @@ export default class Client extends ClientBase {
                 previousUsernames: data.PreviousUserNames
             }, this));
     }
-
+    
     getUserIdFromUsername (username: string): Promise<PartialUser> {
         return this.apis.usersAPI.getUsersByUsernames({
             usernames: [username],
@@ -152,30 +153,30 @@ export default class Client extends ClientBase {
                 }
             });
     }
-
+    
     getUsernameFromUserId (userId: number | string): Promise<PartialUser> {
         if (typeof userId === "string") {
             userId = parseInt(userId);
         }
-
+        
         return this.apis.generalApi.getUserById({
             userId
         })
             .then(data => new PartialUser(data, this));
     }
-
+    
     getUsersByUserIds (userIds: number[] | string[], excludeBannedUsers = false): Promise<PartialUser[]> {
         if (typeof userIds[0] === "string") {
             userIds = (userIds as string[]).map(userId => parseInt(userId));
         }
-
+        
         return this.apis.usersAPI.getUsersByIds({
             excludeBannedUsers,
             userIds: userIds as number[]
         })
             .then(response => response.data.map(userData => new PartialUser(userData, this)));
     }
-
+    
     getUsersByUsernames (usernames: string[], excludeBannedUsers = false): Promise<PartialUser[]> {
         return this.apis.usersAPI.getUsersByUsernames({
             excludeBannedUsers,
